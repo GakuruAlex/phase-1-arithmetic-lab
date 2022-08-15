e814db0c9334aa4909f93a9f170400869784b045
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-15 13:16:48 +0300

    Assignment Complited

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+const num1 =1;
+const num2 =62;
+const num3 =24;
+const num4 =5;
+const num5 =20;
+
+const multiply =num1*num2;
+
+console.log(multiply);
+
+const random =(Math.floor(Math.random()*10)+ 1);
+
+console.log(random);
+
+const mod= num3%num4;
+console.log(mod);
+
+
+const max = Math.max(num5,num1,num4);
+console.log(max);
+
+
+
+
